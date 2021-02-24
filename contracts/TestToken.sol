2a6973940f7af1858d67305ff59682d27a0edcc3
miquelcabot <miquel.cabot@gmail.com>
2021-02-24 23:46:14 +0100

    Add initial data to token

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -2,11 +2,17 @@
 pragma solidity >=0.5.0 <0.8.0;
 
 contract TestToken {
+  string public name = "Test Token";
+  string public symbol = "TEST";
+  string public standard = "Test Token v1.0";
   // Total number of tokens
   uint public totalSupply;
 
+  mapping(address => uint256) public balanceOf;
+
   // Constructor
-  constructor() public {
-    totalSupply = 1000000;
+  constructor(uint _totalSupply) public {
+    totalSupply = _totalSupply;
+    balanceOf[msg.sender] = _totalSupply;
   }
 }
--- test/TestToken.js
+++ test/TestToken.js
@@ -1,10 +1,22 @@
 let TestToken = artifacts.require("TestToken");
 
-contract('TestToken', function(accounts) {
+contract('TestToken', (accounts) => {
+  it ('initializes the contract with the correct values', async () => {
+    let tokenInstance = await TestToken.deployed();
+    let name = await tokenInstance.name();
+    let symbol = await tokenInstance.symbol();
+    let standard = await tokenInstance.standard();
+    assert.equal(name, 'Test Token', 'has the correct name');
+    assert.equal(symbol, 'TEST', 'has the correct symbol');
+    assert.equal(standard, 'Test Token v1.0', 'has the correct standard');
+  })
+
   it('set the total supply upon deployment', async () => {
-    let tokenInstance = await TestToken.deployed()
+    let tokenInstance = await TestToken.deployed();
     let totalSupply = await tokenInstance.totalSupply();
+    let adminBalance = await tokenInstance.balanceOf(accounts[0]);
     assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+    assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account');
   })
 
 })
